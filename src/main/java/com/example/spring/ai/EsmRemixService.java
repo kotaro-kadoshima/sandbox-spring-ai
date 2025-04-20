af230f3e7a4aeac99e770a3219efadd0256f29b6
kotaro-kadoshima <kadoshima.kotaro@gmail.com>
2025-04-20 23:46:42 +0900

    esm接続用のRestClientを作成

--- src/main/java/com/example/spring/ai/EsmRemixService.java
+++ src/main/java/com/example/spring/ai/EsmRemixService.java
@@ -1,10 +1,19 @@
 package com.example.spring.ai;
 
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestClient;
 
 @Service
 public class EsmRemixService {
-    // TODO: 環境変数から取得するようにする
-    private String baseUrl = "https://remix.softbrain.co.jp/";
+
+    private final RestClient restClient;
+
+    public EsmRemixService() {
+        this.restClient = RestClient.builder()
+                .baseUrl("https://remix.softbrain.co.jp/")
+                .defaultHeader("Content-Type", "application/json")
+                .build();
+    }
+
 
 }
