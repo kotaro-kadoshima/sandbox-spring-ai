d20bf1a51b763216f34ea41bdbb34a0e72fce832
kotaro-kadoshima <kadoshima.kotaro@gmail.com>
2025-04-21 23:55:20 +0900

    エンティティ名の一覧を取得を追加

--- src/main/java/com/example/spring/ai/EsmRemixService.java
+++ src/main/java/com/example/spring/ai/EsmRemixService.java
@@ -1,19 +1,33 @@
 package com.example.spring.ai;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestClient;
+
+import java.util.List;
 
 @Service
 public class EsmRemixService {
 
     private final RestClient restClient;
 
-    public EsmRemixService() {
+    public EsmRemixService(@Value("${remix.base-url}") String baseUrl,
+                           @Value("${remix.admin-token}") String adminToken,
+                           @Value("${remix.context_name}") String contextName
+    ) {
         this.restClient = RestClient.builder()
-                .baseUrl("https://remix.softbrain.co.jp/")
+                .baseUrl(baseUrl + "/" + contextName)
                 .defaultHeader("Content-Type", "application/json")
+                .defaultHeader("X-Auth-API-Token", adminToken)
                 .build();
     }
 
+    public void getEntitiesNames() {
+        String endPoint = "/rest/v1/entities/names";
 
+        ResponseEntity<String> res = restClient.get().uri(endPoint).retrieve().toEntity(String.class);
+        System.out.println(res);
+
+    }
 }
